2743f7259bfaf40eebe6ce6a386f32d7878a560c
Your Name <you@example.com>
2026-04-19 20:07:01 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -160,7 +160,7 @@ void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
     
     Pair** old_buckets = map->buckets;
-    hashMap * newMap = createMap(map->capacity * 2);
+    HashMap * newMap = createMap(map->capacity * 2);
 
     for (long i = 0 ; i < map->capacity ; i++) {
         insertMap(newMap->buckets, old_buckets[i]->key, old_buckets[i]->value);
